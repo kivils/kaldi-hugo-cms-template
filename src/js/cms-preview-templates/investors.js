ad9c94acd40f46d42b8e24c6c01f0499d91dc2e2
Julia Sesyunina <idwebkivils@gmail.com>
2017-09-06 17:36:51 +0500

    FIX

--- src/js/cms-preview-templates/investors.js
+++ src/js/cms-preview-templates/investors.js
@@ -6,11 +6,7 @@ export default class PostPreview extends React.Component {
       const {entry, getAsset, widgetFor} = this.props;
 
       return <div>
-        <div className="mw6 ph3 mb3">
-            <h3 className="f3 b lh-title mb2">{entry.getIn(["data", "about", "heading"])}</h3>
-            <div>{entry.getIn(["data", "about", "text"])}</div>
-        </div>
-        <div className="mw6 ph3 mb3">
+        <div className="">
             <div>{ widgetFor("aboutText") }</div>
         </div>
     </div>;
