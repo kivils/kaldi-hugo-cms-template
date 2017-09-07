f656c549dd769130b5fa6b1afc6a18d62903900c
Julia Sesyunina <idwebkivils@gmail.com>
2017-09-07 11:38:34 +0500

    FIX

--- src/js/cms-preview-templates/investors.js
+++ src/js/cms-preview-templates/investors.js
@@ -1,13 +1,13 @@
-import React from "react";
-import format from "date-fns/format";
+import React from 'react';
+import format from 'date-fns/format';
 
 export default class PostPreview extends React.Component {
   render() {
-      const {entry, getAsset, widgetFor, widgetsFor} = this.props;
+      const {entry, getAsset, widgetFor} = this.props;
 
       return <div>
-        <h2>{ entry.getIn(["data", "about", "heading"]) }</h2>
-        <div>{ widgetsFor("text") }</div>
+        <h2>{ entry.getIn(['data', 'about', 'heading']) }</h2>
+        <div>{ widgetFor('text', 'about') }</div>
     </div>;
   }
 }
